535a378a39d2dfaf7050b8ac8d5fda708259268d
KASHISH <kd372744@gmail.com>
2022-10-26 16:12:47 +0530

    Added BoredPetsNFT contract
    For BoredPetsNFT to be a valid NFT, it implements ERC721 standard inheriting the implementation of ERC721URIStorage.sol from @openzeppelin/contracts.
    This implementation of ERC721 is used so that we can store the tokenURIs on chain in storage,which is what allows us to store metadata .

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >0.4.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard{
+    
+
+}
+
